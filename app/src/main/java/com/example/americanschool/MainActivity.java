ff65c6b55556dfcdf3a1b781a0e677cacac099e3
americandev <kubadaev48@gmail.com>
2021-04-19 21:53:13 +0600

    orientashion

--- app/src/main/java/com/example/americanschool/MainActivity.java
+++ app/src/main/java/com/example/americanschool/MainActivity.java
@@ -11,11 +11,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-//    private ConstraintLayout service_language;
-//    private ConstraintLayout service_camp;
-//    private ConstraintLayout service_talking;
-//    private ConstraintLayout service_toefl;
-//    private  TextView callBtn;
+    private ConstraintLayout service_language;
+    private ConstraintLayout service_camp;
+    private ConstraintLayout service_talking;
+    private ConstraintLayout service_toefl;
+    private  TextView callBtn;
     private ConstraintLayout banner;
     int x = 5;
 
